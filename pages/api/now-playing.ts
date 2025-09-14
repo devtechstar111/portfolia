a6abf0bbcd1427b9e298b646c473c3bc54308bc9
henrydevtech <tonyklassen496@gmail.com>
2025-09-14 11:04:08 -0400

    Upgrade npm packages to latest versions
    - Upgraded Next.js from 14.1.0 to 15.5.3
    - Upgraded @headlessui/react to latest (2.2.8)
    - Upgraded @tailwindcss/forms to latest (0.5.10)
    - Upgraded @tailwindcss/typography to latest
    - Upgraded next-themes to latest (0.4.6)
    - Upgraded swr to latest (2.3.6)
    - Upgraded @types/react and @types/react-dom to latest
    - Upgraded @next/bundle-analyzer to latest (15.5.3)
    - Upgraded eslint-config-next to latest (15.5.3)
    - Upgraded rehype-autolink-headings and rehype-slug to latest
    - Fixed Sentry references in now-playing API
    - Kept MDX packages at compatible versions to avoid build issues

--- pages/api/now-playing.ts
+++ pages/api/now-playing.ts
@@ -1,5 +1,4 @@
 import { getNowPlaying } from "@/lib/spotify";
-import { withSentry } from "@sentry/nextjs";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export interface NowPlayingSong {
@@ -43,4 +42,4 @@ async function handler(_: NextApiRequest, res: NextApiResponse<NowPlayingSong>) 
   }
 }
 
-export default withSentry(handler);
+export default handler;
